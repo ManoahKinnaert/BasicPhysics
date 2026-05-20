ad91ebb1d4b1beb3f4be0a6578fb4d41e949c83c
Manoah Kinnaert <manoah.k@icloud.com>
2026-05-20 16:20:54 +0200

    Basic home page

---
+++ BasicPhysics/Scripts/utils/navbar.js
@@ -0,0 +1,7 @@
+async function loadComponent(id, file) {
+  const response = await fetch(file);
+  const text = await response.text();
+  document.getElementById(id).innerHTML = text;
+}
+
+loadComponent("navbar", "./Components/navbar.html");
